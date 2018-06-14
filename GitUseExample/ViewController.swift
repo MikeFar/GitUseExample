5d046993e9193f83fb5889aa1d33e39a5d96b07c
Anthony Maxwell <nymaxa0803@yahoo.com>
2018-06-14 07:34:14 -0400

    I can see more in Xcode Source Editor versus using Git in the Terminal. I am not sure how to see a history of edited changes from Terminal. I only know how to show that the file has not been staged via Git using Terminal, using git status.

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        print("Using Xcode version control\nAlso using Git commands from the terminal for confirmation of my understanding of using Git commands")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
